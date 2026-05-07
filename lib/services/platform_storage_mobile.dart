61a9cf40dfc67f224c5f77303e73fcbfec3aec08
rubiro <vm-tmpl@yandex.ru>
2026-05-07 17:18:15 +0300

    Добавлены платформенные абстракции для Web-версии

---
+++ lib/services/platform_storage_mobile.dart
@@ -0,0 +1,16 @@
+﻿import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'storage_service.dart';
+
+class MobileStorageService implements StorageService {
+  final _storage = const FlutterSecureStorage();
+
+  @override
+  Future<String?> getString(String key) => _storage.read(key: key);
+
+  @override
+  Future<void> setString(String key, String value) =>
+      _storage.write(key: key, value: value);
+
+  @override
+  Future<void> remove(String key) => _storage.delete(key: key);
+}
